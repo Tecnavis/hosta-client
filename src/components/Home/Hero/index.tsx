a1a5c694a250d92cf17e88333423f8c9d43467e9
MUHAMMED SAFVAN MP <muhammedsafvanmp7@gmail.com>
2025-09-24 12:20:25 +0530

    feat: heading chnage

--- src/components/Home/Hero/index.tsx
+++ src/components/Home/Hero/index.tsx
@@ -32,10 +32,10 @@ const Hero: React.FC = () => {
         <div className="container max-w-8xl mx-auto px-5 2xl:px-0 pt-32 md:pt-60 md:pb-68">
           <div className="relative text-white dark:text-dark text-center md:text-start z-10">
             <p className="text-inherit text-xm font-medium">
-              Find your happy place ?
+              Find Your Home ?
             </p>
             <h1 className="text-inherit text-6xl sm:text-9xl font-semibold -tracking-wider md:max-w-45p mt-4 mb-10">
-              safe, comfy, and hassle? free stays!
+              {/* safe, comfy, and hassle? free stays! */} AWAY FROM HOME
             </h1>
             {/* <form className="w-full max-w-full mx-auto px-3">
               <div className="flex flex-row flex-nowrap items-center bg-white/90 dark:bg-dark/60 backdrop-blur-md border border-white/20 dark:border-dark/30 rounded-full overflow-hidden shadow-lg">
